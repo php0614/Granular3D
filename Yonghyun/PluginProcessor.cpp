2a62755f62ba58c932ca76e1fe1d9ae3bf3f7aa5
Yonghyun Kim <51898359+yonghyunk1m@users.noreply.github.com>
2023-05-27 23:20:18 +0900

    White Noise Add & Toggle Working (Plugin)
    Modified the Plugin baseline code as the toggle working and white noise is added when the audio is playing.

---
+++ Yonghyun/PluginProcessor.cpp
@@ -0,0 +1,267 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+int Granular3DAudioProcessor::myState = 0;
+
+//==============================================================================
+Granular3DAudioProcessor::Granular3DAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+    transportSource.addChangeListener (this);
+}
+
+Granular3DAudioProcessor::~Granular3DAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String Granular3DAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool Granular3DAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool Granular3DAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool Granular3DAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double Granular3DAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int Granular3DAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int Granular3DAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void Granular3DAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String Granular3DAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void Granular3DAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void Granular3DAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    transportSource.prepareToPlay(samplesPerBlock, sampleRate);
+    lowPassCoeff = 0.0f;    // [3]
+    sampleCountDown = 0;    // [4]
+}
+
+void Granular3DAudioProcessor::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool Granular3DAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+//bufferToFill이라는 이름의 AudioSourceChannelInfo는 standalone프로그램 일 경우 흔히 쓰이는 변수명임
+//플러그인의 processBlock에서는 그것이 기본 제공되지 않으며 아래를 통해 똑같은 것을 생성해 줄 수 있음.
+//여기서 이것을 만드는 이유는 아래 transportSource가 AudioSourceChannelInfo를 입력형으로 받기 때문
+juce::AudioSourceChannelInfo bufferToFill;
+
+void Granular3DAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    //bufferToFill의 정보를 채우는 과정
+    bufferToFill.buffer      = &buffer;
+    bufferToFill.startSample = 0;
+    bufferToFill.numSamples  = buffer.getNumSamples();
+    //////
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+    
+    //transportSource(juce의 AudioTransport 클래스로 만든 변수)는 프로세서에서 processBlock()을 통해 최종적으로 버퍼를 채워 소리가 플레이 되게 만듬
+    transportSource.getNextAudioBlock(bufferToFill);
+    
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    if (myState == Playing)
+    {
+        for (int channel = 0; channel < totalNumInputChannels; ++channel)
+        {
+            auto* channelData = buffer.getWritePointer(channel);
+
+            // Generate and add white noise to each sample
+            for (int sample = 0; sample < buffer.getNumSamples(); ++sample)
+            {
+                // Generate a random value between -1 and 1
+                const float noiseValue = 0.05 * (juce::Random::getSystemRandom().nextFloat() * 2.0f - 1.0f);
+
+                // Add the white noise to the sample
+                channelData[sample] += noiseValue;
+            }
+        }
+    }
+
+
+}
+
+//==============================================================================
+bool Granular3DAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* Granular3DAudioProcessor::createEditor()
+{
+    return new Granular3DAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void Granular3DAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void Granular3DAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+void Granular3DAudioProcessor::audioFileLoader(juce::File& file){
+    
+    
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new Granular3DAudioProcessor();
+}
+
+
+//일단 현재는 미사용
+void Granular3DAudioProcessor::changeListenerCallback (juce::ChangeBroadcaster* source)
+{
+    if (source == &transportSource)
+    {
+        if (transportSource.isPlaying())
+        {
+            myState = Playing;
+        }
+        else if ((myState == Stopping) || (myState == Playing))
+            myState = Stopped;
+        else if (Pausing == myState)
+            myState = Paused;
+    }
+
+    //Granular3DAudioProcessorEditor::changeListenerCallback();
+//        if (source == &transportSource)
+//        {
+//            if (transportSource.isPlaying())
+//                Granular3DAudioProcessorEditor::changeState (Granular3DAudioProcessorEditor::Playing);
+//            else
+//                changeState (Granular3DAudioProcessorEditor::Stopped);
+//        }
+}
+
+//에디터로부터 트리거 되는 펑션이며, 최종적으로 transportSource(juce의 AudioTransport 클래스로 만든 변수)에 오디오파일을 로드함
+void Granular3DAudioProcessor::setTransportSource(juce::AudioFormatReader* reader){
+    transportSource.stop();
+    transportSource.setSource(nullptr);
+    
+    auto currentAudioFileSource = new juce::AudioFormatReaderSource(reader, true);
+    
+    transportSource.setSource(
+        currentAudioFileSource,
+        0,                   // tells it to buffer this many samples ahead
+        nullptr,        // this is the background thread to use for reading-ahead
+        reader->sampleRate);     // allows for sample rate correction
+    
+};
---
+++ Yonghyun/PluginProcessor.h
@@ -0,0 +1,90 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+enum TransportState
+   {
+       Stopped, //0
+       Starting, //1
+       Playing, //2
+       Pausing, //3
+       Paused, //4
+       Stopping //5
+   };
+
+
+//==============================================================================
+/**
+*/
+class Granular3DAudioProcessor  : public juce::AudioProcessor,
+                                #if JucePlugin_Enable_ARA
+                                public juce::AudioProcessorARAExtension
+                                #endif
+                                public juce::ChangeListener
+{
+public:
+    //==============================================================================
+    Granular3DAudioProcessor();
+    ~Granular3DAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    //오디오 샘플 플레이 관련//////
+    void audioFileLoader(juce::File& file);
+    juce::AudioTransportSource transportSource;
+    //transport를 set할 수 있는 기능들을 함수로 따로 만들었음. 에디터에서 reader를 받아 funtion이 트리거 되며 작동
+    void setTransportSource(juce::AudioFormatReader* reader);
+    ////////////////////////////////////////////
+    void changeListenerCallback (juce::ChangeBroadcaster* source) override;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    
+    static int myState;
+    
+private:
+//    juce::AudioParameterFloat* threshold;
+//    juce::AudioParameterFloat* alpha;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Granular3DAudioProcessor)
+    int sampleCountDown;
+    float lowPassCoeff;
+
+    
+};
